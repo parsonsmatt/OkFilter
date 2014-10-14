86ca728affc345da7c3fe04cd07e70d69d5c22ca
Matt Parsons <parsonsmatt@gmail.com>
2014-10-13 20:04:27 -0400

    Much better usage.

--- OkFilter.rb
+++ OkFilter.rb
@@ -1,14 +1,15 @@
+#! /usr/bin/env ruby
 # OkFilter
 
 require 'highline/import'
 require 'watir-webdriver'
 
 class OkFilter 
-	def initialize(name, pass, lower_bound=60, upper_bound=90)
+	def initialize(name, pass, filter, like)
 		@name = name
 		@pass = pass
-		@lower_bound = lower_bound
-		@upper_bound = upper_bound
+		@filter = filter || 60
+		@like = like || 90
 		@matched = 0
 		@rejected = 0
 		@skipped = 0
@@ -22,7 +23,7 @@ class OkFilter
 		@b.link(:id, "open_sign_in_button").click
 		sleep 1 
 		@b.text_field(:id, 'login_username').set @name
-		@b.text_field(:id, 'login_password').set @pass
+		@b.text_field(:id, 'login_password').set @pass  
 		@b.button(:id, 'sign_in_button').click
 		sleep 3
 	end
@@ -35,15 +36,15 @@ class OkFilter
 	
 	def rate_quickmatch
 		match_percentage = @b.span(:class => 'percent').text.to_i
-		total += 1
-		if match_percentage >= 90
+		@total += 1
+		if match_percentage >= @like
 			@matched += 1
-			b.link(:text => '5 star rating').click
-		elsif match_percentage <= 70
+			@b.link(:text => '5 star rating').click
+		elsif match_percentage <= @filter && match_percentage > 0 # 0 might mean that they haven't filled out their profile yet!
 			@rejected += 1
-			b.link(:text => '1 star rating').click
+			@b.link(:text => '1 star rating').click
 		else
-			b.link(:text => 'Skip').click
+			@b.link(:text => 'Skip').click
 			@skipped += 1
 		end
 
@@ -52,16 +53,32 @@ class OkFilter
 	end
 end
 
-# If running as command line script:
+abort unless __FILE__.include? "OkFilter.rb"
 
-abort unless __FILE__ == "OkFilter.rb"
+help_message =  "Usage: OkFilter.rb [-h][-v][-u username][-F filter_below_match][-L like_above_match]\n\n" +
+		   "-h\tDisplays usage help.\n"+
+		   "-v\tVerbose logging.\n" +
+		   "-u\tUsername. If nothing here, will prompt for it.\n" +
+		   "-F\tFilters matches if their match % is lower than this number. Default 60\n" +
+		   "-L\tLikes matches if their match % is above this number. Default 90\n"
 
-name = ask "What's your OKC name? "
-pass = ask("Password? ") { |q| q.echo = false }
+if ARGV.include? "-h"
+  abort help_message
+end
 
 VERBOSE = ARGV.include? "-v"
+DEFAULT_LOWER = 60
+DEFAULT_UPPER = 90
 
-okc = OkFilter.new name, pass
+name = ARGV[ARGV.index("-u") + 1].to_s if ARGV.include? "-u" || ask("What's your OKC name? ")
+lower = ARGV[ARGV.index("-F")+ 1].to_i if ARGV.include? "-F" || DEFAULT_LOWER
+upper = ARGV[ARGV.index("-L") + 1].to_i if ARGV.include? "-L" || DEFAULT_UPPER
+
+name ||= ask "What's your OKC name? "
+
+pass = ask("Password? ") { |q| q.echo = false }
+
+okc = OkFilter.new name, pass, lower, upper
 
 # Log in:
 okc.login
