3f0eca73caa2b7d293ba1c3c79c737fba17e6e55
Matt Parsons <parsonsmatt@gmail.com>
2014-10-19 16:50:54 -0400

    layout less ugly

--- OkShoes.rb
+++ OkShoes.rb
@@ -2,29 +2,29 @@
 
 require 'OkFilter'
 
-Shoes.app(title: "OkFilter") do
-	stack( margin: 20 ) do
-		title "OkFilter!"
-		para "v0.3.0"
-		flow do
+Shoes.app(title: "OkFilter", width: 350, height: 250) do
+	stack( margin: 20, align: 'right' ) do
+		title "OkFilter!", align: 'center'
+		para "v0.3.0", align: 'center'
+		flow align: 'center' do
 			para "OKC Username:" 
-			@username = edit_line 
+			@username = edit_line :width => 120, :right => 20
 		end 
 		flow do
-			para "Password: "
-			@password = edit_line :secret => true
+			para "Password: " 
+			@password = edit_line :secret => true, :width => 120, :right => 20 
 		end
 		flow do
 			para "Likes matches over: "
-			@like = edit_line.text = "90"
+			@like = edit_line(:width => 80, :right => 20).text = "90"
 		end
 		flow do
 			para "Filter matches under: "
-			@filter = edit_line.text = "60"
+			@filter = edit_line(:width => 80, :right => 20).text = "60"
 		end
-		flow do
-			@go = button "Go!!"
-			@stop = button "Stop!!"
+		flow do 
+			@go = button "Go!!", :width => 80, :left => 40
+			@stop = button "Stop!!", :width => 80, :right => 40
 		end
 	end # end stack
 	# program logic
