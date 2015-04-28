68c9271d0fb2bfb14f4061073f20dfc14c85ae8d
Matt Parsons <parsonsmatt@gmail.com>
2015-04-27 21:38:30 -0400

    indentation

--- OkFilter.rb
+++ OkFilter.rb
@@ -5,90 +5,90 @@ require 'highline/import'
 require 'watir-webdriver'
 
 class OkFilter 
-	def initialize(name, pass, filter, like)
-		@name = name
-		@pass = pass
-		@filter = filter || 60
-		@like = like || 90
-		@matched = 0
-		@rejected = 0
-		@skipped = 0
-		@total = 0
-	end
-	
-	def login 
-		puts "Logging in..." if VERBOSE
-		
-		@b = Watir::Browser.new
-		@b.goto 'http://www.okcupid.com/'
-		@b.link(:id, "open_sign_in_button").click
-		sleep 1 
-		@b.text_field(:id, 'login_username').set @name
-		@b.text_field(:id, 'login_password').set @pass  
-		@b.button(:id, 'sign_in_button').click
-		sleep 3
-	end
-	
-	def goto_quickmatch
-		quickmatch_url = 'http://www.okcupid.com/quickmatch'
-		@b.goto quickmatch_url
-	end
-	
-	def rate_quickmatch
-		match_percentage = @b.span(:class => 'match').text.to_i
-		@total += 1
-		if match_percentage >= @like
-			@matched += 1
-			@b.button(:id => 'quickmatch-like').click
-		elsif match_percentage <= @filter 
-			@rejected += 1
-			@b.button(:id => 'quickmatch-dislike').click
-		else
-			@skipped += 1
-		end
+  def initialize(name, pass, filter, like)
+    @name = name
+    @pass = pass
+    @filter = filter || 60
+    @like = like || 90
+    @matched = 0
+    @rejected = 0
+    @skipped = 0
+    @total = 0
+  end
 
-		puts "Matched: #{@matched}/#{@total} (#{(@matched.to_f/@total*100).to_i}%)\tRejected:#{@rejected}/#{@total} (#{(@rejected.to_f/@total*100).to_i}%)" if VERBOSE
-		sleep 1
-	end
+  def login 
+    puts "Logging in..." if VERBOSE
 
-	def close 
-		@b.close
-	end
+    @b = Watir::Browser.new
+    @b.goto 'http://www.okcupid.com/'
+    @b.link(:id, "open_sign_in_button").click
+    sleep 1 
+    @b.text_field(:id, 'login_username').set @name
+    @b.text_field(:id, 'login_password').set @pass  
+    @b.button(:id, 'sign_in_button').click
+    sleep 3
+  end
+
+  def goto_quickmatch
+    quickmatch_url = 'http://www.okcupid.com/quickmatch'
+    @b.goto quickmatch_url
+  end
+
+  def rate_quickmatch
+    match_percentage = @b.span(:class => 'match').text.to_i
+    @total += 1
+    if match_percentage >= @like
+      @matched += 1
+      @b.button(:id => 'quickmatch-like').click
+    elsif match_percentage <= @filter 
+      @rejected += 1
+      @b.button(:id => 'quickmatch-dislike').click
+    else
+      @skipped += 1
+    end
+
+    puts "Matched: #{@matched}/#{@total} (#{(@matched.to_f/@total*100).to_i}%)\tRejected:#{@rejected}/#{@total} (#{(@rejected.to_f/@total*100).to_i}%)" if VERBOSE
+    sleep 1
+  end
+
+  def close 
+    @b.close
+  end
 end
 
 if  __FILE__ == $0
 
-	help_message = "Usage: OkFilter.rb [-h][-v][-u username][-F filter_below_match][-L like_above_match]\n\n" +
-			   "-h\tDisplays usage help.\n"+
-			   "-v\tVerbose logging.\n" +
-				 "-u\tUsername. If nothing here, will prompt for it.\n" +
-				 "-F\tFilters matches if their match % is lower than this number. Default 60\n" +
-				 "-L\tLikes matches if their match % is above this number. Default 90\n"
+  help_message = "Usage: OkFilter.rb [-h][-v][-u username][-F filter_below_match][-L like_above_match]\n\n" +
+    "-h\tDisplays usage help.\n"+
+    "-v\tVerbose logging.\n" +
+    "-u\tUsername. If nothing here, will prompt for it.\n" +
+    "-F\tFilters matches if their match % is lower than this number. Default 60\n" +
+    "-L\tLikes matches if their match % is above this number. Default 90\n"
 
-	if ARGV.include? "-h"
-		abort help_message
-	end
+  if ARGV.include? "-h"
+    abort help_message
+  end
 
-	VERBOSE = ARGV.include? "-v"
-	DEFAULT_LOWER = 60
-	DEFAULT_UPPER = 90
+  VERBOSE = ARGV.include? "-v"
+  DEFAULT_LOWER = 60
+  DEFAULT_UPPER = 90
 
-	name = ARGV[ARGV.index("-u") + 1].to_s if ARGV.include? "-u" || ask("What's your OKC name? ")
-	lower = ARGV[ARGV.index("-F")+ 1].to_i if ARGV.include? "-F" || DEFAULT_LOWER
-	upper = ARGV[ARGV.index("-L") + 1].to_i if ARGV.include? "-L" || DEFAULT_UPPER
+  name = ARGV[ARGV.index("-u") + 1].to_s if ARGV.include? "-u" || ask("What's your OKC name? ")
+  lower = ARGV[ARGV.index("-F")+ 1].to_i if ARGV.include? "-F" || DEFAULT_LOWER
+  upper = ARGV[ARGV.index("-L") + 1].to_i if ARGV.include? "-L" || DEFAULT_UPPER
 
-	name ||= ask "What's your OKC name? "
+  name ||= ask "What's your OKC name? "
 
-	pass = ask("Password? ") { |q| q.echo = false }
+  pass = ask("Password? ") { |q| q.echo = false }
 
-	okc = OkFilter.new name, pass, lower, upper
+  okc = OkFilter.new name, pass, lower, upper
 
-	# Log in:
-	okc.login
+  # Log in:
+  okc.login
 
-	# Start rating!
-	while true do
-		okc.goto_quickmatch
-        okc.rate_quickmatch
-	end
+  # Start rating!
+  while true do
+    okc.goto_quickmatch
+    okc.rate_quickmatch
+  end
 end
--- OkShoes.rb
+++ OkShoes.rb
@@ -3,51 +3,51 @@
 require_relative './OkFilter'
 
 Shoes.app(title: "OkFilter", width: 350, height: 250) do
-	stack( margin: 20, align: 'right' ) do
-		title "OkFilter!", align: 'center'
-		para "v0.3.0", align: 'center'
-		flow align: 'center' do
-			para "OKC Username:" 
-			@username = edit_line :width => 120, :right => 20
-		end 
-		flow do
-			para "Password: " 
-			@password = edit_line :secret => true, :width => 120, :right => 20 
-		end
-		flow do
-			para "Likes matches over: "
-			@like = edit_line(:width => 80, :right => 20).text = "90"
-		end
-		flow do
-			para "Filter matches under: "
-			@filter = edit_line(:width => 80, :right => 20).text = "60"
-		end
-		flow do 
-			@go = button "Go!!", :width => 80, :left => 40
-			@stop = button "Stop!!", :width => 80, :right => 40
-		end
-	end # end stack
+  stack( margin: 20, align: 'right' ) do
+    title "OkFilter!", align: 'center'
+    para "v0.3.0", align: 'center'
+    flow align: 'center' do
+      para "OKC Username:" 
+      @username = edit_line :width => 120, :right => 20
+    end 
+    flow do
+      para "Password: " 
+      @password = edit_line :secret => true, :width => 120, :right => 20 
+    end
+    flow do
+      para "Likes matches over: "
+      @like = edit_line(:width => 80, :right => 20).text = "90"
+    end
+    flow do
+      para "Filter matches under: "
+      @filter = edit_line(:width => 80, :right => 20).text = "60"
+    end
+    flow do 
+      @go = button "Go!!", :width => 80, :left => 40
+      @stop = button "Stop!!", :width => 80, :right => 40
+    end
+  end # end stack
 
-	# program logic
-	@keep_going = true
-	
-	@stop.click do
-		@keep_going = false;
-	end
-	
-	@go.click do
-		okc = OkFilter.new @username.text, 
-						   @password.text,
-						   @filter.to_i,
-						   @like.to_i
-		Thread.new do
-			@keep_going = true;	 
-			okc.login
-			okc.goto_quickmatch
-			while @keep_going do
-				okc.rate_quickmatch
-			end
-			okc.close
-		end
-	end
+  # program logic
+  @keep_going = true
+
+  @stop.click do
+    @keep_going = false;
+  end
+
+  @go.click do
+    okc = OkFilter.new @username.text, 
+      @password.text,
+      @filter.to_i,
+      @like.to_i
+    Thread.new do
+      @keep_going = true;	 
+      okc.login
+      okc.goto_quickmatch
+      while @keep_going do
+        okc.rate_quickmatch
+      end
+      okc.close
+    end
+  end
 end
